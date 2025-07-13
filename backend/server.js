55c5578c0343e39702db99c03d9089b0cdb32908
Adityagupta0973 <adityag0908@gmail.com>
2025-07-13 19:08:30 +0530

    Changed Server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://vidyoday-dhadpad.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true, // only if you use cookies or Authorization headers
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {});
